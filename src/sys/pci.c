195d4ffb446542b49924d86191b4bacc6a18c74e
kurkii <kurkiie@protonmail.com>
2024-11-11 15:30:16 +0100

    PCI WIP

--- src/sys/pci.c
+++ src/sys/pci.c
@@ -1,12 +1,27 @@
 #include <lord84.h>
 #include <stdio.h>
 #include "acpi.h"
+#include "../mm/vmm.h"
+#include "pci.h"
+
+#define PCIE_CONF_SPACE_WIDTH       4096
+#define PCI_CONF_SPACE_WIDTH        256
+
+#define PCI_HEADER_TYPE_GENERAL     0x0     // General device
+#define PCI_HEADER_TYPE_PCI2PCI     0x1     // PCI to PCI bridge
+#define PCI_HEADER_TYPE_PCI2CB      0x2     // PCI to CardBus bridge
+
+#define ID_REGISTER                 0x0     // Vendor ID, 0-15; Device ID, 16-31
 
 mcfg_t *mcfg;
 
+uint64_t config_space_base_addr = 0;
+uint8_t start_pci_num = 0;
+uint8_t end_pci_num = 0;
+
 extern uint64_t hhdmoffset;
 
-uint64_t *parse_conf_space(){
+void parse_conf_space(){
     uint64_t num = (mcfg->header.length - sizeof(desc_header_t)) / sizeof(conf_space_t);
 
     kprintf("addr: 0x{xn}", (uint64_t)mcfg);
@@ -21,9 +36,44 @@ uint64_t *parse_conf_space(){
         kprintf(" PCI segment group: {dn}", header.pci_seg_group);
         kprintf(" start pci bus number: 0x{xn}", header.start_pci_num);
         kprintf(" end pci bus number: 0x{xn}", header.end_pci_num);
-    }
 
-    return NULL;
+        config_space_base_addr = header.base_ecm + hhdmoffset;
+        start_pci_num = header.start_pci_num;
+        end_pci_num = header.end_pci_num;
+    }
+}
+
+void enumerate_conf_space(){
+    /* Counter for the while loop below. We know that the PCIe configuration space is max
+       4096 bytes wide, so we make sure we dont go over that */
+    uint64_t counter = 0;
+
+    pci_header_0_t *header = (pci_header_0_t*)config_space_base_addr;
+
+    while(counter < PCIE_CONF_SPACE_WIDTH){
+
+        /* If Vendor ID is 0xffff, that means that that device does not exist */
+        if(header->vendor_id == 0xffff){
+            break;
+        }
+
+        kprintf("pci: vendor_id: 0x{x} class: 0x{x} subclass: 0x{x} header type: 0x{xn}", header->vendor_id, header->class_code, header->subclass, header->header_type);
+        
+        switch (header->header_type) {
+            case PCI_HEADER_TYPE_GENERAL:
+                counter += sizeof(pci_header_0_t);
+                header = (pci_header_t*)((uint64_t)header + sizeof(pci_header_0_t));
+                break;
+            case PCI_HEADER_TYPE_PCI2PCI:
+                counter += sizeof(pci_header_1_t);
+                header = (pci_header_t*)((uint64_t)header + sizeof(pci_header_1_t));
+                break;
+            default:
+                kprintf("uhoh\n");
+                return;
+        }
+
+    }
 }
 
 void pci_init(){
@@ -38,4 +88,14 @@ void pci_init(){
     mcfg = (mcfg_t*)((uint64_t)mcfg + hhdmoffset);
 
     parse_conf_space();
+
+    /* Map the base address to virtual memory so we can access it */
+    extern uint64_t *kernel_page_map;
+
+    vmm_map_page(kernel_page_map, config_space_base_addr, config_space_base_addr - hhdmoffset, PTE_BIT_PRESENT | PTE_BIT_RW | PTE_BIT_NX);
+
+    pci_header_t *header = (pci_header_t*)config_space_base_addr;
+    kprintf("Vendor ID: 0x{xn}", header->vendor_id);
+
+    enumerate_conf_space();
 }
--- src/sys/pci.h
+++ src/sys/pci.h
@@ -1,1 +1,68 @@
+#include <stdint.h>
 void pci_init();
+
+typedef struct pci_header_t {
+    uint16_t vendor_id;
+    uint16_t device_id;
+    uint16_t command;
+    uint16_t status;
+    uint8_t revision_id;
+    uint8_t prog_if;
+    uint8_t subclass;
+    uint8_t class_code;
+    uint8_t cache_line_size;
+    uint8_t latency_timer;
+    uint8_t header_type;
+    uint8_t bist;
+}__attribute((packed)) pci_header_t;
+
+typedef struct pci_header_0_t {
+    pci_header_t header;
+    uint32_t bar0;
+    uint32_t bar1;
+    uint32_t bar2;
+    uint32_t bar3;
+    uint32_t bar4;
+    uint32_t bar5;
+    uint32_t cardbus_cis_ptr;
+    uint16_t subsytem_vendor_id;
+    uint16_t subsystem_id;
+    uint32_t expansion_rom_base;
+    uint8_t capabilities_ptr;
+    uint8_t reserved1;
+    uint16_t reserved2;
+    uint32_t reserved3;
+    uint8_t interrupt_line;
+    uint8_t interrupt_pin;
+    uint8_t min_grant;
+    uint8_t max_latency;
+}__attribute((packed)) pci_header_0_t;
+
+typedef struct pci_header_1_t {
+    pci_header_t header;
+    uint32_t bar0;
+    uint32_t bar1;
+    uint8_t primary_bus_number;
+    uint8_t secondary_bus_number;
+    uint8_t subordinate_bus_number;
+    uint8_t secondary_latency_timer;
+    uint8_t io_base;
+    uint8_t io_limit;
+    uint16_t secondary_status;
+    uint16_t memory_base;
+    uint16_t memory_limit;
+    uint16_t prefetch_base_;
+    uint16_t prefetch_limit;
+    uint32_t prefetch_base_upper;
+    uint32_t prefetch_limit_upper;
+    uint16_t io_base_upper;
+    uint16_t io_limit_upper;
+    uint8_t capability_ptr;
+    uint8_t reserved1;
+    uint16_t reserved2;
+    uint32_t expansion_rom_base;
+    uint8_t interrupt_line;
+    uint8_t interrupt_pin;
+    uint16_t bridge_control;
+
+}__attribute((packed)) pci_header_1_t;
